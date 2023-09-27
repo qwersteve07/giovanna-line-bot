6f5c07bca5b830450d269cab37d36ef32510ff7e
SteveLee <qwersteve07@gmail.com>
2023-09-27 16:26:51 +0800

    feat: 採用 yaml 作為資料輸入源

--- linebot/router.js
+++ linebot/router.js
@@ -1,9 +1,12 @@
 const https = require("https");
 const Router = require("@koa/router");
 const router = new Router();
+const yaml = require("js-yaml");
+const fs = require("fs");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const TOKEN =
-  "hQPr0Gavcur1dAUQ4g/YC4BsLPL1ibUoQlZW3XxKhX5VFx3yJYd0kBx+DyVkq8+NRFYqHjJM0R1gnzkHmI4mikmqDlciNNl9Q0UT1LPq46MLTYD5amNDopOCEC9sKByAY+mqnZV69/PI/wiZ7J2OKQdB04t89/1O/w1cDnyilFU=";
+let lock = false;
 
 router.get("/", () => {
   ctx.body = "index";
@@ -23,81 +26,111 @@ router.post("/webhook", async (ctx) => {
     let messages = [];
     const { text } = message;
 
-    const mentionText = ["嫂子", "gio", "余盈希"];
+    const mentionText = ["嫂子", "gio", "giovanna", "盈希", "余盈希"];
 
     const selectedMentionText = mentionText.find((x) => text.includes(x));
 
-    if (text.match(`${selectedMentionText} 問 `)) {
-      const question = text.replace(`${selectedMentionText} 問 `, "");
-      const reply = await fetch("http://localhost:3003/query", {
+    if (selectedMentionText) {
+      // 阻止嫂子一直講話
+      if (!lock) {
+        lock = true;
+        setTimeout(() => {
+          lock = false;
+        }, 180000);
+      } else {
+        // webhook verification
+        ctx.status = 200;
+        return;
+      }
+
+      if (text.match(`${selectedMentionText} 自我介紹`)) {
+        messages = [
+          {
+            type: "text",
+            text: `大家好，我是${selectedMentionText}，曾在上海工作 2+ 年，並有 5 年跨域管理經驗，涉及商業分析、產品設計、維持與優化顧客體驗以及用戶運營，並任職過工研院、壽險、教育科技，現職於出行科技產業。`,
+          },
+        ];
+      } else if (text.match(`${selectedMentionText} 問 `)) {
+        const question = text.replace(`${selectedMentionText} 問 `, "");
+        const reply = await fetch("http://localhost:3003/query", {
+          method: "POST",
+          body: JSON.stringify({ query: question }),
+          headers: {
+            "content-type": "application/json",
+          },
+        }).then((res) => {
+          if (!res.ok) {
+            messages = [
+              {
+                type: "text",
+                text: "我現在腦袋有點亂，讓我整理一下",
+              },
+            ];
+            throw new Error(res.statusText);
+          }
+          return res.text();
+        });
+
+        messages = [
+          {
+            type: "text",
+            text: "要懂的自主思考，不要只是聽別人說，好嗎？",
+          },
+          {
+            type: "text",
+            text: reply,
+          },
+        ];
+      } else if (selectedMentionText) {
+        const data = yaml.load(
+          fs.readFileSync(`${__dirname}/replies.yml`, "utf8")
+        ).Replies;
+        const index = Math.floor(Math.random() * data.length);
+
+        messages = [
+          {
+            type: "text",
+            text: data[index],
+          },
+        ];
+      }
+
+      const dataString = JSON.stringify({
+        replyToken,
+        messages,
+      });
+
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.LINEBOT_TOKEN,
+      };
+
+      // Options to pass into the request, as defined in the http.request method in the Node.js documentation
+      const webhookOptions = {
+        hostname: "api.line.me",
+        path: "/v2/bot/message/reply",
         method: "POST",
-        body: JSON.stringify({ query: question }),
-        headers: {
-          "content-type": "application/json",
-        },
-      }).then((res) => {
-        if (!res.ok) {
-          throw new Error(res.statusText);
-        }
-        return res.text();
+        headers: headers,
+        body: dataString,
+      };
+
+      const request = https.request(webhookOptions, (res) => {
+        res.on("data", (d) => {
+          process.stdout.write(d);
+        });
       });
 
-      messages = [
-        {
-          type: "text",
-          text: "要懂的自主思考，不要只是聽別人說。",
-        },
-        {
-          type: "text",
-          text: reply,
-        },
-      ];
-    } else if (mentionText.some((item) => text.includes(item))) {
-      //  receive message from backend
+      // Handle error
+      // request.on() is a function that is called back if an error occurs
+      // while sending a request to the API server.
+      request.on("error", (err) => {
+        console.error(err);
+      });
 
-      messages = [
-        {
-          type: "text",
-          text: "Emma 你東西處理好了沒？",
-        },
-      ];
+      // Finally send the request and the data we defined
+      request.write(dataString);
+      request.end();
     }
-
-    const dataString = JSON.stringify({
-      replyToken,
-      messages,
-    });
-
-    const headers = {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + TOKEN,
-    };
-
-    // Options to pass into the request, as defined in the http.request method in the Node.js documentation
-    const webhookOptions = {
-      hostname: "api.line.me",
-      path: "/v2/bot/message/reply",
-      method: "POST",
-      headers: headers,
-      body: dataString,
-    };
-
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d);
-      });
-    });
-
-    // Handle error
-    // request.on() is a function that is called back if an error occurs
-    // while sending a request to the API server.
-    request.on("error", (err) => {
-      console.error(err);
-    });
-
-    // Finally send the request and the data we defined
-    request.write(dataString);
-    request.end();
   }
 });
 
